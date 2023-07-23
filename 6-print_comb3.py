d173316e80ba3a761e7ff253f7d6e0a61a290198
Precious Banigo <banigoprecious@yahoo.com>
2023-07-23 19:17:26 +0100

    Python Hello World

---
+++ 6-print_comb3.py
@@ -0,0 +1,7 @@
+for a in range(9):
+    for b in range(a+1, 10):
+        if a==8 and b==9:
+            print("{}{}".format(a,b))
+        else:
+            print("{}{}".format(a,b), end=",")
+            
