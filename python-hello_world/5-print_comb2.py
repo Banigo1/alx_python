96ee131b28e093c9645c16f8c9da9b0e69faf487
Precious Banigo <banigoprecious@yahoo.com>
2023-07-26 11:27:09 +0100

    updated

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -2,4 +2,4 @@ for i in range(0, 100):
         if i == 99:
             print(i ,end="")
         else:
-            print('{:02}'.format(i), end=",")
+            print('{:02}'.format(i), end=", ")
