1371a6f879046b7405aa3f5b4c5c270fe7160aae
Precious Banigo <banigoprecious@yahoo.com>
2023-07-26 03:43:05 +0100

    updated

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -7,7 +7,3 @@ elif number == 0:
     print(f"{number} is zero")
 elif number < 0:
      print(f"{number} is negative")
-<<<<<<< HEAD:2-positive_or_negative.py
-     
-=======
->>>>>>> 916c2f87a8c5135c2c2c677bb23f6dce421ce312:python-hello_world/2-positive_or_negative.py
