6091659c1bf6d21042d34e03639f8fc38643b0da
Precious Banigo <banigoprecious@yahoo.com>
2023-08-03 19:17:24 +0100

    Add files via upload

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,12 @@
+from sys import argv
+if __name__ == "__main__":
+ def print_args(arg):
+  """Prints the number of and the list of its arguments."""
+  args = arg.argv
+  num_args = len(args)
+
+  print("Number of arguments:", num_args)
+  print("List of arguments:")
+  for arg in args:
+    print("  ", arg)
+  print_args()
