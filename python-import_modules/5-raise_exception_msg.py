0e753e6bb84bafa0edb8761bf120997d440f8734
Precious Banigo <banigoprecious@yahoo.com>
2023-08-04 14:59:49 +0100

    exception

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -3,5 +3,7 @@ raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
 
 try:
     raise_exception_msg("C is fun")
+    raise_exception_msg("Python is cool")
+    raise_exception_msg("")
 except NameError as ne:
     print(ne)
