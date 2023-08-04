2edef89076d30c333e5b84c686f43848e9173835
Precious Banigo <banigoprecious@yahoo.com>
2023-08-04 05:15:31 +0100

    update

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+import importlib.util
+
+def import_variable_from_file(file_name, variable_name):
+    spec = importlib.util.spec_from_file_location("module_name", file_name)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return getattr(module, variable_name)
+
 if __name__ == "__main__":
- from variable_load_2 import a
- print(a)
+    variable_value = import_variable_from_file("variable_load_2.py", "a")
+    print("The value of 'a' is:", variable_value)
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,13 @@
-if __name__ == "__main__":
- def raise_exception_msg(message=""):
-    raise NameError(message)
-try:
-    raise_exception_msg()
-except NameError as ne:
-    print(ne)
+def raise_exception_msg(message=""):
+  """Raises a name exception with a message.
+
+  Args:
+    message: The message to be included in the exception.
+
+  Raises:
+    NameError: The exception raised.
+  """
+
+  if message == "":
+    message = "No message provided."
+  raise NameError(message)
