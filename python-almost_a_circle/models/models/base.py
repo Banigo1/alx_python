85e5154f21e35086d3ca47d0c1ca50cb7a2f91ac
Precious Banigo <banigoprecious@yahoo.com>
2023-08-06 18:40:56 +0100

    commit

--- python-almost_a_circle/models/models/base.py
+++ python-almost_a_circle/models/models/base.py
@@ -10,6 +10,25 @@ class Base:
 
 from models.base import Base
 
+if __name__ == "__main__":
+
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
+    
+    
+    """
 # Create instances of Base
 obj1 = Base()
 obj2 = Base()
@@ -19,3 +38,5 @@ obj3 = Base(id=10)
 print(obj1.id)  # Output: 1
 print(obj2.id)  # Output: 2
 print(obj3.id)  # Output: 10
+
+    """
