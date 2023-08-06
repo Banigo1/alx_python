868eb7a982dc6ebbc5e8acb188fbf06c26f25d04
Precious Banigo <banigoprecious@yahoo.com>
2023-08-06 17:56:08 +0100

    Base Class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,3 +7,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+from models.base import Base
+
+# Create instances of Base
+obj1 = Base()
+obj2 = Base()
+obj3 = Base(id=10)
+
+# Print the ids of the objects
+print(obj1.id)  # Output: 1
+print(obj2.id)  # Output: 2
+print(obj3.id)  # Output: 10
