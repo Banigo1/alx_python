94f910d7c2ff55097e1d96d9a625e9c2977f162d
Precious Banigo <banigoprecious@yahoo.com>
2023-08-09 03:11:39 +0100

    ...

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -11,13 +11,4 @@ def is_prime(number):
             return False
     
     # No divisor found, number is prime; return True
-    return True
-
-"""
-def is_prime(n):
-    for i in range(2,n):
-        if (n%i) == 0:
-            return False   
-    return True         
-
-"""                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    return True                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
