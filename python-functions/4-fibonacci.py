f0bd4e1dd3971ab3a7fab18efe8c6bdf968b5b7f
Precious Banigo <banigoprecious@yahoo.com>
2023-08-09 22:32:43 +0100

    .

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,14 +1,26 @@
 # Fibonacci Sequence Function
-
-def fibonacci_sequence(n):
+def fibo(n):
+    if n == 1:
+        return 1
+        
+    elif n == 0:
+        return 0
+    
+    elif n > 1:
+        return fibo(n-1) + fibo(n-2)
+        
+       
     #Returns a list of the first n Fibonacci numbers.
-    if n == 0:
-        return []
-        fibonacci_numbers =[0]
-    elif n <= 1:
-        fibonacci_numbers=[0]
-    else:
-        fibonacci_numbers = [0, 1]
-        for i in range(2, n):
-            fibonacci_numbers.append(fibonacci_numbers[i-1]+fibonacci_numbers[i-2])
-    return(fibonacci_numbers )
+    def fibonacci_sequence(t):
+        lis = []
+    for i in range():
+        lis.append(fibo(i))
+        return lis
+    
+    def main():
+        num = int(input())
+        print(fibonacci_sequence(num))
+        pass
+    
+    if __name__ == "__main__":
+        main()
