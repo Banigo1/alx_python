1f157ce87585241f5848c73838858a161dd22433
Precious Banigo <banigoprecious@yahoo.com>
2023-08-09 03:17:43 +0100

    ...

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -3,7 +3,7 @@
 def fibonacci_sequence(n):
     if n <= 0:
         return []
-    fibonacci_numbers = [int]
+    fibonacci_numbers = [0, 1]
     for i in range(2, n):
         fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
 
