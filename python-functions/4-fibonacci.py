c10a7b812c7ec2b5172e8545794844bd65ce9650
Precious Banigo <banigoprecious@yahoo.com>
2023-08-09 03:13:48 +0100

    ...

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,8 @@
 # Fibonacci Sequence Function
 
 def fibonacci_sequence(n):
-    
     if n <= 0:
-        return []
-
+        return [0]
     fibonacci_numbers = [0, 1]
     for i in range(2, n):
         fibonacci_numbers.append(fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2])
