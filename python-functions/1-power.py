1e1ec469b84dbaa6327908dfa532ea81fd3b016b
Precious Banigo <banigoprecious@yahoo.com>
2023-08-09 01:53:57 +0100

    recursion

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,12 +1,16 @@
+
+import sys
+sys.setrecursionlimit(10000)
 def pow(a, b):
+  
   """Computes a to the power of b and returns the value."""
+  
   if b == 0:
     return 1
   elif b == 1:
     return a
   else:
     return a * pow(a, b - 1)
-
 
 if __name__ == "__main__":
   print(pow(2, 3))  # 2^3 = 8
