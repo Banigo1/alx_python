9ee90e769e276cb0f9db4b3096467ad368d06d9b
Precious Banigo <banigoprecious@yahoo.com>
2023-08-10 08:21:07 +0100

    ...

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,82 +1,64 @@
-
-# Python - Almost a circle - # 1. First Rectangle Project only
-
-"""
-A class to represent a rectangle.
-
-This class has the following public methods:
-
-* width: Gets the width of the rectangle.
-* width: Sets the width of the rectangle.
-* height: Gets the height of the rectangle.
-* height: Sets the height of the rectangle.
-* x: Gets the x-coordinate of the rectangle.
-* x: Sets the x-coordinate of the rectangle.
-* y: Gets the y-coordinate of the rectangle.
-* y: Sets the y-coordinate of the rectangle.
- 
-"""
-
 from models.base import Base
-
 
 class Rectangle(Base):
     """A class to represent a rectangle."""
 
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes the rectangle."""
         super().__init__(id)
-        self._width = width
-        self._height = height
-        self._x = x
-        self._y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
     @property
     def width(self):
         """Gets the width of the rectangle."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, width):
         """Sets the width of the rectangle."""
         if width < 0:
             raise ValueError("Width must be non-negative.")
-        self._width = width
+        self.__width = width
 
     @property
     def height(self):
         """Gets the height of the rectangle."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, height):
         """Sets the height of the rectangle."""
         if height < 0:
             raise ValueError("Height must be non-negative.")
-        self._height = height
+        self.__height = height
 
     @property
     def x(self):
         """Gets the x-coordinate of the rectangle."""
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, x):
         """Sets the x-coordinate of the rectangle."""
         if x < 0:
             raise ValueError("X-coordinate must be non-negative.")
-        self._x = x
+        self.__x = x
 
     @property
     def y(self):
         """Gets the y-coordinate of the rectangle."""
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, y):
         """Sets the y-coordinate of the rectangle."""
         if y < 0:
             raise ValueError("Y-coordinate must be non-negative.")
-        self._y = y
-        
+        self.__y = y
