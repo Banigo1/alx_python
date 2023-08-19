4fe1a2b3c8babc901e433ba5f0640cc037ed7f8c
Precious Banigo <banigoprecious@yahoo.com>
2023-08-19 20:00:08 +0100

    ...

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,12 +1,2 @@
 def raise_exception_msg(message=""):
     raise Exception(message)
-
-try:
-    raise_exception_msg("c is fun")
-except Exception as e:
-    print("Exception:", e)
-
-try:
-    raise_exception_msg("python is cool")
-except Exception as e:
-    print("Exception:", e)
