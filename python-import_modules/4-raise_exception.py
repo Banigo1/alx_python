043c1407fe500c0869d04e46e4bad50f5fc2633e
Precious Banigo <banigoprecious@yahoo.com>
2023-08-19 19:57:14 +0100

    ...

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
         raise TypeError("This is a custom type exception.")
-    
-    except TypeError as te:
-        
-        print("Exception raised:", te)
