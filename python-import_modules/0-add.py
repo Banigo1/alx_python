301c866d9dac2a3176b198fe4a75a45416fa887f
Precious Banigo <banigoprecious@yahoo.com>
2023-08-19 16:11:03 +0100

    ...

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 
 from add_0 import add
-
-    """My addition function
-         Args:
-         a is the first integer
-         b is the second integer
-
-    Returns:
-        The return value. a + b
-    """
- if __name__ == "__main__":
+if __name__ == "__main__":
         a = 1
         b = 2
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
