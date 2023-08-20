6910d99b0ee827511d4811854a5292006b635eb4
Precious Banigo <banigoprecious@yahoo.com>
2023-08-20 09:27:12 +0100

    ...

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
-
 """
-This script lists all states from the database hbtn_0e_0_usa
-The script uses two modules: MySQLdb and sys.
-MySQLdb is a Python interface to the MySQL database. It is used to connect to a MySQL database and execute 
-SQL queries
-
+Script that lists all states from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
-import sys
+from sys import argv
 
-
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
+if __name__ == "__main__":
+    # Connect to a MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
     
-"""
-MySQLdb.connect() method is used to establish a connection to the MySQL database. 
-
-"""
-conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=dbname,
-                           charset="utf8")
-cur = conn.cursor()
-
-"""     
-    The cursor.execute() method is used to execute an 
-    SQL query that selects all rows from the states table 
-    and orders them by the id column in ascending order.
-"""
-
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
+    # Execute the query
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    
+    # Fetch all the rows
+    rows = cur.fetchall()
+    
+    # Print the rows
+    for row in rows:
         print(row)
-cur.close()
-conn.close()
     
-    
-    
+    # Close the cursor and the connection to the database
+    cur.close()
+    db.close()
