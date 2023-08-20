d6bf59c7c22a2aba5b2b33d7bd046e26cd487f3a
Precious Banigo <banigoprecious@yahoo.com>
2023-08-20 08:40:16 +0100

    ...

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-import importlib.util
-
-def import_variable_from_file(file_name, variable_name):
-    spec = importlib.util.spec_from_file_location("module_name", file_name)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return getattr(module, variable_name)
-
 if __name__ == "__main__":
-    variable_value = import_variable_from_file("variable_load_2.py", "a")
-    print("The value of 'a' is:", variable_value)
+    from variable_load_5 import a
+
+    print(a)
