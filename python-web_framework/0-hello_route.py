5d684f9f13126acc66050599ec0da0170e6ec87e
Precious Banigo <banigoprecious@yahoo.com>
2023-08-26 21:42:53 +0100

    ...

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,12 +2,12 @@
 This module implements a Flask web application.
 
 The web application listens on
-0.0.0.0, port 5000 and provides the following 
+0.0.0.0, port 5000 and provides the following
 routes:
 - /: Displays "Hello HBNB!"
 
 Usage:
-    To start the web application, 
+    To start the web application,
     run this module directly:
         $ python app.py
 
@@ -19,10 +19,11 @@ from flask import Flask
 app = Flask(__name__)
 
 # Define a route for the root URL ("/")
+
 @app.route('/', strict_slashes=False)
- 
+
 def hello_hbnb():
-    
+
     # Return the desired response
     return 'Hello HBNB!'
 
