d46169990aac8ea802f76163becde3cd610bf38e
Precious Banigo <banigoprecious@yahoo.com>
2023-08-27 00:55:35 +0100

    ...

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -4,8 +4,10 @@ This script creates a Flask web application that listens on
 It has five routes:
 /, /hbnb, /c/<text>, /python/(<text>), and /number/<n>, which display
 "Hello HBNB!", "HBNB", "C" followed by the value of the text variable
-(with underscores replaced by spaces), "Python" followed by the value of the text variable
-(with underscores replaced by spaces), and "n is a number" if n is an integer, respectively.
+(with underscores replaced by spaces), 
+"Python" followed by the value of the text variable
+(with underscores replaced by spaces), and "n is a number" 
+if n is an integer, respectively.
 The default value of text for the /python/(<text>) route is "is cool".
 The option strict_slashes=False is used in the 
 route definitions to allow for URLs with or without a trailing slash.
@@ -14,17 +16,21 @@ route definitions to allow for URLs with or without a trailing slash.
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
