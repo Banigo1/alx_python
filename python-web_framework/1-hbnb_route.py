c9ff8808cd368f563557d546e96556f8fbdb750a
Precious Banigo <banigoprecious@yahoo.com>
2023-08-27 00:01:40 +0100

    ...

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -17,14 +17,14 @@ Usage:
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 
 def hello():
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
 
+@app.route('/hbnb', strict_slashes=False)
 
 def hbnb():
     return 'HBNB'
