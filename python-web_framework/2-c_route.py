12f54e43bafde4755ec107d372a695d7a1afcc3e
Precious Banigo <banigoprecious@yahoo.com>
2023-08-27 00:15:15 +0100

    ...

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -7,7 +7,7 @@ routes:
 
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed by the value of the text variable 
+/c/<text>: display “C ” followed by the value of the text variable
 (replace underscore _ symbols with a space )
 
 
@@ -20,13 +20,16 @@ Usage:
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
