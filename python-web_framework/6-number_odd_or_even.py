1f75658fa44c75cb0333317b6b44117233977652
Precious Banigo <banigoprecious@yahoo.com>
2023-08-28 10:28:58 +0100

    ...

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -1,3 +1,27 @@
+"""
+This script starts a Flask web application that listens on
+0.0.0.0, port 5000.
+
+The option strict_slashes=False is used in all
+route definitions to allow for trailing slashes in the URLs.
+
+
+It has the following routes:
+
+/: displays “Hello HBNB!”
+/hbnb: displays “HBNB”
+/c/<text>: displays "C ", followed by the value of the
+text variable (replace underscore _ symbols with a space)
+/python/(<text>): displays "Python", followed by the value of
+the text variable (replace underscore _ symbols with a space).
+    The default value of text is “is cool”.
+/number/<n>: displays “n is a number” only if n is an integer.
+/number_template/<n>: display a HTML page only if n is an integer:
+        H1 tag: “Number: n” inside the tag BODY
+/number_odd_or_even/<n>: display a HTML page only if n is an integer:
+        H1 tag: “Number: n is even|odd” inside the tag BODY
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
