56f4951aaaa0a46166c48a7ae706f74f3a4174e6
Precious Banigo <banigoprecious@yahoo.com>
2023-08-28 08:02:58 +0100

    ...

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,6 +1,7 @@
 """
 This script creates a Flask web application that listens on 0.0.0.0, port 5000.
-It has two routes: / and /hbnb, which display "Hello HBNB!" and "HBNB", respectively.
+It has two routes: / and /hbnb, which display
+"Hello HBNB!" and "HBNB", respectively.
 The option strict_slashes=False is used in the
 route definitions to allow for URLs with or without a trailing slash.
 Is there anything else you would like to know? 😊
