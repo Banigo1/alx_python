c6774a560a20d5dae6a4bcafd86a0c3566fd92a9
Precious Banigo <banigoprecious@yahoo.com>
2023-08-28 11:38:21 +0100

    ...

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -16,7 +16,7 @@ It has the following routes:
     /number/<n>: display “n is a number” only if n is an integer
     /number_template/<n>: display a HTML page only if n is an integer:
     H1 tag: “Number: n” inside the BODY tag.
-    
+
 The option strict_slashes=False is used in all
 route definitions to allow for trailing slashes in the URLs.
 
