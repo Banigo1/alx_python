a6e455a71af04c5e1302a0f111b1d5b3872c3831
Precious Banigo <banigoprecious@yahoo.com>
2023-08-28 10:36:37 +0100

    ...

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -26,18 +26,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -45,13 +49,16 @@ def python(text):
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     return '{} is a number'.format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return '<html><body><h1>Number: {}</h1></body></html>'.format(n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
