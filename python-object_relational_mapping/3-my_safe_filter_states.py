d4f2bf5efb6ff0adbcef01f777852bad2283b71c
Precious Banigo <banigoprecious@yahoo.com>
2023-08-29 09:28:13 +0100

    ...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,23 +17,25 @@ and id safety for given name for a table using MySQLdb
 import MySQLdb
 import sys
 if __name__ == "__main__":
+    # connect to the MySQL database using provided argument
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
     
-    
+    # Create a cursor object
     cur = db.cursor()
     
-    
+    # Execute SQL query to select data from `states` table
     cur.execute("SELECT DISTINCT id,\
                 name FROM states WHERE name = %s\
                 ORDER BY id ASC", [sys.argv[4]])
-
+    
+    # Fetch all rows returned by the query
     rows = cur.fetchall()
     
-    
+    # Loop over rows
     for r in rows:
         
-        
+        # print all result
         print(r)
