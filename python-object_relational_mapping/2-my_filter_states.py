233e9a181fe9d8f983879a32060bb94d1002962e
Precious Banigo <banigoprecious@yahoo.com>
2023-08-29 06:39:53 +0100

    ...

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -53,9 +53,9 @@ if __name__ == "__main__":
                          db=database)
     cur = db.cursor()
     
-    query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
-
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
+            state_name)
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
