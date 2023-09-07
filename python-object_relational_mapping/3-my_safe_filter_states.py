5c30df0c3195b4db0c06aeacf65057ae144244b0
Precious Banigo <banigoprecious@yahoo.com>
2023-09-07 11:53:03 +0100

    ...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,6 @@ takes 4 arguments:
     mysql password,
     database name and
     state name searched (safe from MySQL injection),
-    
 connects to a MySQL server running on localhost at port 3306,
 Sorts result in ascending order by states.id, and prints name
 and id safety for given name for a table using MySQLdb
@@ -22,20 +21,15 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-    
     # Create a cursor object
     cur = db.cursor()
-    
     # Execute SQL query to select data from `states` table
     cur.execute("SELECT DISTINCT id,\
                 name FROM states WHERE name = %s\
                 ORDER BY id ASC", [sys.argv[4]])
-    
     # Fetch all rows returned by the query
     rows = cur.fetchall()
-    
     # Loop over rows
     for r in rows:
-        
         # print all result
         print(r)
