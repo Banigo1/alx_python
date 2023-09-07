394c3d1bf59f068c191d800da264b5c0f74aa301
Precious Banigo <banigoprecious@yahoo.com>
2023-09-07 20:59:22 +0100

    ...

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,7 +6,8 @@ from model_state import Base, State
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print(
-        "Usage: python script.py <mysql_username><mysql_password> <database_name>")
+"Usage: python script.py <mysql_username><mysql_password> <database_name>"
+)
         sys.exit(1)
 
     mysql_username = sys.argv[1]
@@ -15,14 +16,21 @@ if __name__ == "__main__":
 
     # Create a database connection
     engine = create_engine(
-    f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
+    f"mysql+mysqldb://{
+        mysql_username}:{
+        mysql_password}@localhost:3306/{
+        database_name
+        }"
+                )
 
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query for State objects containing the letter 'a' and order by states.id
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    states_with_a = session.query(
+        State).filter(
+            State.name.like('%a%')).order_by(State.id)
 
     # Print the results
     for state in states_with_a:
@@ -30,3 +38,4 @@ if __name__ == "__main__":
 
     # Close the session
     session.close()
+    
