13634d476b7d620acbcc44ebf34415bf08052a49
Precious Banigo <banigoprecious@yahoo.com>
2023-09-07 20:29:05 +0100

    ...

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,37 +1,35 @@
-#!/usr/bin/python3
-""" script that lists all State objects that contain the letter a from the...
-    ...database hbtn_0e_6_usa """
+#!/usr/bin/env python3
+"""
+Script that lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa
+"""
 
-
-if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(State.name.
-                                                                like('%a%')):
-        print("{}: {}".format(state.id, state.name))
-    session.close()#!/usr/bin/python3
-""" script that lists all State objects that contain the letter a from the...
-    ...database hbtn_0e_6_usa """
-
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Check for correct number of arguments
+    if len(sys.argv) != 4:
+        print("Usage: python3 list_states.py <mysql_username> <mysql_password> <database_name>")
+        sys.exit(1)
 
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).filter(State.name.
-                                                                like('%a%')):
-        print("{}: {}".format(state.id, state.name))
+    # Get command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine and session
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Retrieve all State objects that contain the letter 'a' and sort them by id
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print(state.name)
+
+    # Close the session
     session.close()
