3d0a5a74a10dfcbcd5afe2c9eb1b751b22e31b87
Precious Banigo <banigoprecious@yahoo.com>
2023-09-07 20:50:27 +0100

    ...

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,7 +5,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
+        print(
+        "Usage: python script.py <mysql_username><mysql_password> <database_name>")
         sys.exit(1)
 
     mysql_username = sys.argv[1]
@@ -13,7 +14,8 @@ if __name__ == "__main__":
     database_name = sys.argv[3]
 
     # Create a database connection
-    engine = create_engine(f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
+    engine = create_engine(
+    f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
 
     # Create a session
     Session = sessionmaker(bind=engine)
