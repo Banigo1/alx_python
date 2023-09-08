6f298bcdecb2b71f5e73a2432594e66fdf16d578
Precious Banigo <banigoprecious@yahoo.com>
2023-09-08 10:45:59 +0100

    ...

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-this script lists all states from the database hbtn_0e_6_usa using SQLAlchemy
+This script lists all states from the database hbtn_0e_6_usa using SQLAlchemy
 """
 import sys
 from sqlalchemy import create_engine
