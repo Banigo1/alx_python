aa980f6f0be5421c7250d01ffb3c1f7573161a21
Precious Banigo <banigoprecious@yahoo.com>
2023-09-08 10:29:02 +0100

    ...

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,15 +8,13 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password>
-              <database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password><database>".format(sys.argv[0]))
         sys.exit(1)
 
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     # Create a MySQL database connection
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        username, password, database)
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, database)
     engine = create_engine(db_url)
 
     # Create a session to interact with the database
@@ -24,8 +22,7 @@ if __name__ == "__main__":
     session = Session()
 
     # Query the database for State objects containing 'a' and sort by states.id
-    states_with_a = session.query(State).filter(State.name.like('%a%')
-                                                ).order_by(State.id)
+    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
 
     # Print the results in the specified format
     for state in states_with_a:
@@ -33,4 +30,3 @@ if __name__ == "__main__":
 
     # Close the session
     session.close()
-
