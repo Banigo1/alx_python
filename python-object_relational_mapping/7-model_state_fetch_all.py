a0ab15d8b5d52d5c7e010b28a30a5db24e435869
Precious Banigo <banigoprecious@yahoo.com>
2023-09-11 00:10:16 +0100

    7 and 9

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 """
 Script that lists all State objects from the database hbtn_0e_6_usa
 """
@@ -21,3 +22,21 @@ if __name__ == '__main__':
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
+=======
+
+# This script lists all State objects from the database hbtn_0e_6_usa
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localho\
+st/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{:d}: {:s}".format(instance.id, instance.name))
+>>>>>>> 886299c7cfdd3cc2a6d62c312a1fabb4f72358db
--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,33 +1,29 @@
-#!/usr/bin/python3
-"""List all State objects that contain the
-letter 'a' from the database hbtn_0e_6_usa."""
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    # Create a MySQL database connection
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, database)
-    engine = create_engine(db_url)
+    # Create a database connection
+    engine = create_engine(f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}")
 
-    # Create a session to interact with the database
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database for State objects containing 'a' and sort by states.id
+    # Query for State objects containing the letter 'a' and order by states.id
     states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
 
-    # Print the results in the specified format
+    # Print the results
     for state in states_with_a:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
     # Close the session
     session.close()
