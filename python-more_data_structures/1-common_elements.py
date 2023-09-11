55c7e95ef738e66a508587db16d71e2558b6e0ef
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 00:08:46 +0100

    ...

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -2,16 +2,7 @@
 def common_elements(set_1, set_2):
 
     result = []  # empty list
-    
-    for element in set_1:  # checks for all elements in set 1 to see if it is also present in set_2.
-        
-        if element in set_2:  # checks if the current element from set_1 (element)
-        # is also present in set_2. If it is, it means that the element is common to both sets.
-        
-            result.append(element)  # adds matching elements to result.
-            
-        return result   # returns the result list, which contains all the common 
-                        # elements found between set_1 and set_2.
-
-    # return set_1 && set_2 OR return set_1 and set_2 will also work.
-    # return set_1.intersection(set_2) can also works in the statement above
+    for element in set_1:  # checks through all elements of set_1
+        if element in set_2:  # checks if the current element from set_1 is also present in set_2
+            result.append(element)  # adds matching elements to the result list
+    return result  # returns result list, which contains all the common elements found  between set_1 and set_2
