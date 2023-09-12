fd405cfc807da6ba6c107f3579abdc08ee4ea46d
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 18:36:02 +0100

    ...

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,5 +5,51 @@ Instantiation with size (no type/value verification)
 
 """
 class Square:
-    """ Square class """
-    pass
+    
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        get_size(): Get the size of the square.
+        set_size(size: int): Set the size of the square.
+        area(): Calculate the area of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    def get_size(self):
+        """
+        Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    def set_size(self, size):
+        """
+        Set the size of the square.
+
+        Args:
+            size (int): The new size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
