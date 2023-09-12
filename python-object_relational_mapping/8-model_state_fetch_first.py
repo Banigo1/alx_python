548c82829f35e851d01eaa245b9b8dc6833c8f0f
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 12:10:38 +0100

    PEP8 validation

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"""   script that prints the first State object from the database...
-    ...hbtn_0e_6_usa """
+"""
+Script that lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
-if __name__ == "__main__":
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    # Get the command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    session = Session(engine)
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
+    # Create the connection URL
+    connection_url = f'mysql+mysqldb://{mysql_username
+    }:{mysql_password
+    }@localhost:3306/{
+        database_name}'
+
+    # Create the engine and session
+    engine = create_engine(connection_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the database for State objects containing the letter 'a'
+    query = session.query(State).filter(State.name.like('%a%')
+                                        ).order_by(State.id).all()
+
+    # Print the results
+    for state in query:
+        print(f'{state.id}: {state.name}')
+
+    # Close the session
     session.close()
