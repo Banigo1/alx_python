c9215d7570615a67498dea1ff3fc365b2db7064c
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 17:08:36 +0100

    ...

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects that contain the letter 'a'
+Script to list all State objects that contain the letter 'a'
 from the database hbtn_0e_6_usa
 """
 
@@ -10,26 +10,32 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-if __name__ == '__main__':
-    # Get the command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+def list_states_with_letter_a(username, password, database_name):
+    """List all State objects containing the letter 'a'"""
+    # Create a database connection using SQLAlchemy
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database_name))
 
-    # Create the connection URL
-    connection_url = f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}'
-
-    # Create the engine and session
-    engine = create_engine(connection_url)
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database for State objects containing the letter 'a'
-    query = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    # Query the database to find State objects containing the letter 'a'
+    # and order by states.id
+    states_with_a = session.query(State)\
+        .filter(State.name.like('%a%'))\
+        .order_by(State.id)\
+        .all()
 
     # Print the results
-    for state in query:
-        print(f'{state.id}: {state.name}')
+    for state in states_with_a:
+        print("{}: {}".format(state.id, state.name))
 
-    # Close the session
-    session.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: {} <username> <password> <database_name>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database_name = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states_with_letter_a(username, password, database_name)
