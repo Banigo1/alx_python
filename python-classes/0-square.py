fb08741003ade4bd658f2c4420a56afa113eda91
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 19:06:24 +0100

    ...

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-""" This module contains an empty class named Square,
-that defines a square by a
-private instance attribute: size
-"""
-class Square:
-    
-    """
-    A class that defines a square.
+""" 
+    This module contains an empty class named Square,
+    that defines a square by a
+    private instance attribute: size
 
     Attributes:
         __size (int): The size of the square.
 
-    Methods:
-        get_size(): Get the size of the square.
-        set_size(size: int): Set the size of the square.
-        area(): Calculate the area of the square.
-    """
-
+"""
+class Square:
     def __init__(self, size):
-        """
-        Initialize a square with a given size.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
