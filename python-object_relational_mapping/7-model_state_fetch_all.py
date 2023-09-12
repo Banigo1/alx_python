643801368991e58447963f331f4ea3e5eb9565b3
Precious Banigo <banigoprecious@yahoo.com>
2023-09-12 11:52:07 +0100

    SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-Script that lists all State objects from the database hbtn_0e_6_usa
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password,
-                                   dbname), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
-=======
 
 # This script lists all State objects from the database hbtn_0e_6_usa
 
@@ -39,4 +15,4 @@ st/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = Session()
     for instance in session.query(State).order_by(State.id):
         print("{:d}: {:s}".format(instance.id, instance.name))
->>>>>>> 886299c7cfdd3cc2a6d62c312a1fabb4f72358db
+
