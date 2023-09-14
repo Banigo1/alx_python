4322933ac2b87a15998ed05db4707418eb13cb41
Precious Banigo <banigoprecious@yahoo.com>
2023-09-14 09:02:19 +0100

    ...

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,10 +6,6 @@ class Square:
 
     Methods:
         __init__(self, size=0): Initializes a Square instance with an optional size.
-        area(self): Calculates and returns the area of the square.
-        size(self): Getter property for retrieving the size of the square.
-        size(self, value): Setter property for setting the size of the square.
-       
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
@@ -27,20 +23,17 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-    def area(self):
-       
-        return self.__size ** 2
-
     @property
     def size(self):
-       
         return self.__size
 
     @size.setter
     def size(self, value):
-   
+        # size(self, value): Setter property for setting the size of the square.
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
+        # TypeError: If size is not an integer.
         if value < 0:
             raise ValueError("size must be >= 0")
+        # ValueError: If size is less than 0.
         self.__size = value
