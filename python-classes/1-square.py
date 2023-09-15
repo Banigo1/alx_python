33ce43a8a81f4b797669c2920f3bb6f92fa8f668
Precious Banigo <banigoprecious@yahoo.com>
2023-09-15 07:30:55 +0100

    ...

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,25 +1,19 @@
 """
-This module defines a Square class.
+This module defines a Square class and a Private instance attribute: size,
+Instantiation with optional size: def __init__(self, size=0),
+size must be an integer, otherwise raise a TypeError exception with the
+message size must be an integer. If size is less than 0, raise a
+ValueError exception with the message size must be >= 0
 
-Private instance attribute: size
-Instantiation with optional size: def __init__(self, size=0)
-size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-if size is less than 0, raise a ValueError exception with the message size must be >= 0
 """
 
 class Square:
     """
-    The Square class defines a square.
+    The Square class
 
     Attributes:
         __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes a Square instance with an optional size.
-        area(self): Calculates and returns the area of the square.
-        size(self): Getter property for retrieving the size of the square.
-        size(self, value): Setter property for setting the size of the square.
-
+       
     """
     def __init__(self, size=0):
         """
