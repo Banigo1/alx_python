311f42bddf4c3e5eada8ecd37cc441d301ea1d47
Precious Banigo <banigoprecious@yahoo.com>
2023-09-15 06:54:59 +0100

    ...

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -38,43 +38,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        """
-        Calculate and return the area of the square.
-
-        Returns:
-            int: The area of the square.
-
-        """
-        return self.__size ** 2
-
-    @property
-    def size(self):
-        """
-        Get the size of the square.
-
-        Returns:
-            int: The size of the square.
-
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        Set the size of the square.
-
-        Args:
-            value (int): The new size of the square.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+ 
