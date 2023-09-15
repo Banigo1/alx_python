217054af2fe7242c0fca1c6dc9037c9ccf2a773e
Precious Banigo <banigoprecious@yahoo.com>
2023-09-15 08:12:32 +0100

    Python - Classes and Objects

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,3 +1,17 @@
+"""
+This module defines a Square class.
+
+Private instance attribute: size
+Property def size(self): to retrieve it
+Property setter def size(self, value): to set it:
+size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+if size is less than 0, raise a ValueError exception with the message size must be >= 0
+Instantiation with optional size: def __init__(self, size=0):
+Public instance method: def area(self): that returns the current square area
+Public instance method: def my_print(self): that prints in stdout the square with the character #:
+if size is equal to 0, print an empty line.
+
+"""
 class Square:
     """
     This class defines a square.
