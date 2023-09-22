a9da840a2a4b2069787ec2a8730c3c441a17952d
Precious Banigo <banigoprecious@yahoo.com>
2023-09-22 21:57:29 +0100

    ...

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,47 +1,36 @@
-class BaseGeometry:
-    def __init__(self):
-        pass
-class Rectangle(BaseGeometry):
+#!/usr/bin/python3
+"""
+    This is a base class
+"""
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+class TypeMetaClass(type):
+    """
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+
+
+class Rectangle(BaseGeometry, metaclass=TypeMetaClass):
+    """
+    This is a sub-class of the baseclass
+    """
     def __init__(self, width, height):
-        super().__init__()
-        self.__width = width
-        self.__height = height
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        super().__init__()
+        """
+        function sets the values width and height and ensures
+        """
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
 
-    def __validate_integer(self, value):
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("Width and height must be positive integers.")
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        super().__init__()
-        self.__validate_integer(width)
-        self.__validate_integer(height)
-        self.__width = width
-        self.__height = height
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        super().__init__()
-        self.__validate_integer(width)
-        self.__validate_integer(height)
-        self.__width = width
-        self.__height = height
-class BaseGeometry:
-    def __init__(self):
-        pass
-
-
-class Rectangle(BaseGeometry):
-    def __init__(self, width, height):
-        super().__init__()
-        self.__validate_integer(width)
-        self.__validate_integer(height)
-        self.__width = width
-        self.__height = height
-
-    def __validate_integer(self, value):
-        if not isinstance(value, int) or value <= 0:
-            raise ValueError("Width and height must be positive integers.")
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
