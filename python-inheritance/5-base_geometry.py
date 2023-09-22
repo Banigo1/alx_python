aeaa0d47a5d0e6e21455175e9c030574fc605ef4
Precious Banigo <banigoprecious@yahoo.com>
2023-09-22 17:38:12 +0100

    BaseGeometry():

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module has a class and a public
+instance method Public instance method:
+def area(self): that raises an Exception.
+"""
+class BaseGeometry():
+    """Class BaseGeometry.
+    It has a method, area, which is
+    not implemented yet.
+
+    """
+    # def __init__(self):
+    #     pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
