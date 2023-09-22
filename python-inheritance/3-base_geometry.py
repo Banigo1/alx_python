fe6fcaf573890f10d7e621cea057ce4aa75b9e60
Precious Banigo <banigoprecious@yahoo.com>
2023-09-22 18:33:14 +0100

    ...

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
+"""
+    This is a base class
+"""
 
-"""This module has an empty class BaseGeometry"""
+class TypeMetaClass(type):
+    """
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
 
-class BaseGeometry():
-    """Class BaseGeometry"""
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+class BaseGeometry(metaclass=TypeMetaClass):
+    """
+    This is a base class
+    """
     pass
+
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attributes = super().__dir__()
+
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
