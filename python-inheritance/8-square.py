ac5d91e7bd0bd038dc2f5a22a2caf061554850c3
Precious Banigo <banigoprecious@yahoo.com>
2023-09-22 22:02:04 +0100

    ...

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,122 +1,143 @@
-
+#!/usr/bin/python3
 """
-     A class used to represent a Rectangle
+This module defines several classes for geometry calculations.
 
-    ...
+class TypeMetaClass(type):
+    This is a metaclass used to represent the class type in order to eliminate
+    the inherited method __init_subclass__ from BaseGeometry.
 
-    Attributes
-    ----------
-    width : int
-        the width of the rectangle
-    height : int
-        the height of the rectangle
+    Methods:
+    __dir__(cls) -> None:
+        Exclude attribute __init_subclass__ in dir().
 
-    Methods
-    -------
-    area():
-        Returns the area of the rectangle
-    integer_validator(name, value):
-        Validates that the value is a positive integer
-        
+class BaseGeometry(metaclass=TypeMetaClass):
+    This is a base class for geometric shapes.
+
+    Methods:
+    __dir__(cls) -> None:
+        Exclude attribute __init_subclass__ in dir().
+    area(self):
+        Raises an exception with an error message.
+        This method should be overridden
+        in subclasses to provide the area calculation.
+    integer_validator(self, name, value):
+        Validates whether the value is an integer and greater than 0,
+        otherwise it raises an error.
+
+class Rectangle(BaseGeometry):
+    This class represents a rectangle, a subclass of BaseGeometry.
+
+    Methods:
+    __init__(self, width, height):
+        Initializes the Rectangle object with given width and height.
+    area(self):
+        Returns the area of the rectangle.
+    __str__(self):
+        Returns a formatted string representation of the
+        Rectangle's dimensions.
+
+class Square(Rectangle):
+    This class represents a square, a subclass of Rectangle.
+
+    Methods:
+    __init__(self, size):
+        Initializes the Square object with the given size.
+    area(self):
+        Returns the area of the square.
+    __str__(self):
+        Returns a formatted string representation of the Square's size.
 """
 
-class Rectangle:
+
+class TypeMetaClass(type):
     """
-    A class used to represent a Rectangle
-
-    ...
-
-    Attributes
-    ----------
-    width : int
-        the width of the rectangle
-    height : int
-        the height of the rectangle
-
-    Methods
-    -------
-    area():
-        Returns the area of the rectangle
-    integer_validator(name, value):
-        Validates that the value is a positive integer
+    This is a metaclass used to represent the class type inorder to eliminate
+    the inherited method init subclass from basegeometry
     """
-
-    def __init__(self, width, height):
+    def __dir__(cls) -> None:
         """
-        Parameters
-        ----------
-        width : int
-            The width of the rectangle
-        height : int
-            The height of the rectangle
+        Exclude attribute init subclass in dir()
         """
+        attr = super().__dir__()
+        return [attr for attr in attr if attr != '__init_subclass__']
 
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+
+class BaseGeometry(metaclass=TypeMetaClass):
+    """
+    This is a base class
+    """
+    def __dir__(cls) -> None:
+        """
+        Exclude attribute init subclass in dir()
+        """
+        attr = super().__dir__()
+        return [attr for attr in attr if attr != '__init_subclass__']
 
     def area(self):
-        """Returns the area of the rectangle"""
+        """
+        Function that raise an exception with an error message
+        """
+        raise Exception("area() is not implemented")
 
+    def integer_validator(self, name, value):
+        """
+        Function validates whether the value is an integer and
+        is greater than 0, otherwise it raises an error
+        """
+        self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        self.value = value
+
+
+class Rectangle(BaseGeometry):
+    """
+    This is a sub-class of the baseclass
+    """
+    def __init__(self, width, height):
+        """
+        function sets the values width and height and ensures
+        """
+        super().integer_validator("width", width)
+        self._Rectangle__width = width
+        super().integer_validator("height", height)
+        self._Rectangle__height = height
+
+    def area(self):
+        """
+        function that returns the area of the rectangle
+        """
         return self.__width * self.__height
 
-    @staticmethod
-    def integer_validator(name, value):
+    def __str__(self):
         """
-        Validates that the value is a positive integer
-
-        Parameters
-        ----------
-        name : str
-            The name of the attribute to be validated
-        value : int
-            The value of the attribute to be validated
-
-        Raises
-        ------
-        TypeError
-            If value is not an integer
-        ValueError
-            If value is not greater than 0
+        Returns a formatted string representation of the Rectangle's dimensions
         """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
 
 class Square(Rectangle):
     """
-    A class used to represent a Square, inherits from Rectangle class
-
-    ...
-
-    Attributes
-    ----------
-    size : int
-        the size of the square (both width and height)
-
-    Methods
-    -------
-    area():
-        Returns the area of the square
+    This is a sub-class of the Rectangle class
     """
 
     def __init__(self, size):
         """
-        Parameters
-        ----------
-        size : int
-            The size of the square (both width and height)
+        function sets the value size and ensures it's a positive integer
         """
-
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self._Square__size = size
 
     def area(self):
-        """Returns the area of the square"""
+        """
+        function that returns the area of the rectangle
+        """
+        return self._Square__size ** 2
 
-        return self.__size ** 2
+    def __str__(self):
+        """
+        Returns a formatted string representation of the object size
+        """
+        return f"[Square]{self._Square__size ** 2}"
