860896f9464af6514a934f076207f61e8bc8649e
Precious Banigo <banigoprecious@yahoo.com>
2023-09-26 12:50:22 +0100

    ...

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,23 +1,40 @@
+#!/usr/bin/python3
+"""
+This is a Python script that takes in a URL and an email address, 
+sends a POST request to the passed URL with the email as a parameter, 
+and finally displays the body of the response.
+
+The email must be sent in the variable email
+Uses the packages requests and sys
+"""
 import requests
 import sys
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <URL> <email>")
-        return
+def check_email(url, email):
+    """
+    Sends a POST request to the given URL with the email as a parameter
+    and displays the response body.
 
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    payload = {'email': email}
-
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to include in the request.
+    """
     try:
+        payload = {'email': email}
         response = requests.post(url, data=payload)
-        print("Response Body:")
-        print(response.text)
-    except requests.RequestException as e:
-        print("Error:", e)
+        response.raise_for_status()
+
+        context = response.text
+
+        print(context)
+
+    except requests.exceptions.RequestException as e:
+        print("Error occurred:", e)
 
 if __name__ == "__main__":
-    main()
-#!/usr/bin/python3
+    url = sys.argv[1]
+    email = sys.argv[2]
+    check_email(url, email)
+
+
+        
