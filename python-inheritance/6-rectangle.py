8381ac564b9a68d37ea2d7b860f8745ceb29a1d4
Precious Banigo <banigoprecious@yahoo.com>
2023-10-01 23:22:59 +0100

    Python - Inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,24 +1,20 @@
+
 #!/usr/bin/python3
 """
-Creates a Rectangle class.
+A module that contains the class Rectangle which inherits from BaseGeometry
 """
-BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
-    Private instance attributes:
-        - width
-        - height
-    Inherits from BaseGeometry.
+    """
+    A class that defines a Recatngle that inherits from BaseGeometry
     """
 
     def __init__(self, width, height):
-        """Initializes an instance.
-        Args:
-            - width: width of the rectangle
-            - heigth: height of the rectangle
-        """
+        """Instantiation method"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
