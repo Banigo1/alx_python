ac6848a7ea9602a92da4091e001eac56714258bf
Precious Banigo <banigoprecious@yahoo.com>
2023-10-02 01:46:06 +0100

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,15 +2,51 @@ import requests
 import csv
 import sys
 
-def export_employee_todo_list_to_csv(employee_id):
-    """Exports the employee's TODO list to a CSV file."""
-    try:
-        # Get the employee's TODO list items.
-        todo_list_items_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
-        todo_list_items_response = requests.get(todo_list_items_url)
-        todo_list_items_response.raise_for_status()  # Raise an exception for bad responses.
-        todo_list_items = todo_list_items_response.json()
+def get_employee_info(employee_id):
+    """Get employee details from the API."""
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(employee_url)
+    response.raise_for_status()  # Raise an exception for bad responses.
+    return response.json()
 
+def get_todo_list(employee_id):
+    """Get employee's TODO list from the API."""
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(todo_url)
+    response.raise_for_status()  # Raise an exception for bad responses.
+    return response.json()
+
+def display_todo_progress(employee_id):
+    """Display TODO list progress for a given employee."""
+    try:
+        # Get employee details
+        employee_info = get_employee_info(employee_id)
+        employee_name = employee_info["name"]
+
+        # Get TODO list
+        todo_list = get_todo_list(employee_id)
+
+        # Count completed and total tasks
+        completed_tasks = sum(1 for task in todo_list if task["completed"])
+        total_tasks = len(todo_list)
+
+        # Display output
+        print(f"Employee {employee_name} is done with tasks ({completed_tasks}/{total_tasks}):")
+
+        # Display titles of completed tasks
+        for task in todo_list:
+            if task["completed"]:
+                print(f"\t{task['title']}")
+
+        # Export TODO list to CSV
+        export_todo_list_to_csv(employee_id, employee_info, todo_list)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching information: {e}")
+
+def export_todo_list_to_csv(employee_id, employee_info, todo_list):
+    """Export TODO list to a CSV file."""
+    try:
         # Open a CSV file for writing.
         csv_file_path = f"{employee_id}.csv"
         with open(csv_file_path, "w", newline="") as csv_file:
@@ -20,19 +56,19 @@ def export_employee_todo_list_to_csv(employee_id):
             # Write the header row to the CSV file.
             csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-            # Iterate over the employee's TODO list items, and write each item to the CSV file.
-            for todo_list_item in todo_list_items:
+            # Write TODO list items to the CSV file.
+            for task in todo_list:
                 csv_writer.writerow([
                     employee_id,
-                    todo_list_item["username"],
-                    todo_list_item["completed"],
-                    todo_list_item["title"]
+                    employee_info["username"],
+                    task["completed"],
+                    task["title"]
                 ])
 
         print(f"TODO list for employee {employee_id} exported to {csv_file_path}.")
 
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching TODO list for employee {employee_id}: {e}")
+    except IOError as e:
+        print(f"Error exporting TODO list to CSV: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -40,5 +76,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-
-    export_employee_todo_list_to_csv(employee_id)
+    display_todo_progress(employee_id)
