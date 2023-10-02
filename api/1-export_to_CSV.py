ec62a44beaff9fbe92a79042daeeea3cfa4fe292
Precious Banigo <banigoprecious@yahoo.com>
2023-10-02 01:10:09 +0100

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,52 +1,40 @@
-    """
-    This module would create a CSV file called 1.csv in the current directory,
-    containing the employee's TODO list items.
-    """
-import csv
+"""This module Records all tasks that are owned by this employee
+    and exports the data in the CSV format."""
+
 import requests
-import sys
+import csv
 
-# No execution of this file when imported
-if __name__ == "__main__":
-    
-    # Pass employee id on command line
-    id = sys.argv[1]
+def export_employee_todo_list_to_csv(employee_id):
+    """Exports the employee's TODO list to a CSV file."""
 
-    # APIs 
-    userTodoURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    userProfile = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    # Get the employee's TODO list items.
+    todo_list_items_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    todo_list_items_response = requests.get(todo_list_items_url)
+    todo_list_items = todo_list_items_response.json()
 
-    # Make requests on APIs
-    todoResponse = requests.get(userTodoURL)
-    profileResponse = requests.get(userProfile)
+    # Open a CSV file for writing.
+    csv_file = open(f"{employee_id}.csv", "w")
 
-    # Parse responses and store in variables
-    todoJson_Data = todoResponse.json()
-    profileJson_Data = profileResponse.json()
+    # Create a CSV writer object.
+    csv_writer = csv.writer(csv_file)
 
-    # Get employee information
-    employeeName = profileJson_Data['name']
+    # Write the header row to the CSV file.
+    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    # Count total and completed tasks
-    totalTasks = 0
-    completedTasks = 0
+    # Iterate over the employee's TODO list items, and write each item to the CSV file.
+    for todo_list_item in todo_list_items:
+        csv_writer.writerow([
+            employee_id,
+            todo_list_item["username"],
+            todo_list_item["completed"],
+            todo_list_item["title"]
+        ])
 
-    for data in todoJson_Data: # Each dict in variable data
-        for key, value in data.items():
-            if key == 'completed':
-                totalTasks += 1
-                if value == True:
-                    completedTasks += 1
+    # Close the CSV file.
+    csv_file.close()
 
-    print("Employee {} is done with "
-          "tasks({}/{}):".format(employeeName, completedTasks, totalTasks))
+# Pass employee id on command line.
+employee_id = sys.argv[1]
 
-    # Retrieve title of completed tasks and write to CSV file
-    with open('{}.csv'.format(id), 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for data in todoJson_Data: # Each dict in variable data
-            for key, value in data.items():
-                if key == 'completed' and value == True:
-                    writer.writerow([id, employeeName, value, data['title']])
-                    print("\t {}".format(data['title']))
+# Export the employee's TODO list to a CSV file.
+export_employee_todo_list_to_csv(employee_id)
