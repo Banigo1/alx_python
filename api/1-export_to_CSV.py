7a74849619ad189c47d51074b58d44d7740ac638
Precious Banigo <banigoprecious@yahoo.com>
2023-10-02 01:33:00 +0100

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,44 @@
-"""This module Records all tasks that are owned by this employee
-    and exports the data in the CSV format."""
-
 import requests
 import csv
+import sys
 
 def export_employee_todo_list_to_csv(employee_id):
     """Exports the employee's TODO list to a CSV file."""
+    try:
+        # Get the employee's TODO list items.
+        todo_list_items_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+        todo_list_items_response = requests.get(todo_list_items_url)
+        todo_list_items_response.raise_for_status()  # Raise an exception for bad responses.
+        todo_list_items = todo_list_items_response.json()
 
-    # Get the employee's TODO list items.
-    todo_list_items_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
-    todo_list_items_response = requests.get(todo_list_items_url)
-    todo_list_items = todo_list_items_response.json()
+        # Open a CSV file for writing.
+        csv_file_path = f"{employee_id}.csv"
+        with open(csv_file_path, "w", newline="") as csv_file:
+            # Create a CSV writer object.
+            csv_writer = csv.writer(csv_file)
 
-    # Open a CSV file for writing.
-    csv_file = open(f"{employee_id}.csv", "w")
+            # Write the header row to the CSV file.
+            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
 
-    # Create a CSV writer object.
-    csv_writer = csv.writer(csv_file)
+            # Iterate over the employee's TODO list items, and write each item to the CSV file.
+            for todo_list_item in todo_list_items:
+                csv_writer.writerow([
+                    employee_id,
+                    todo_list_item["username"],
+                    todo_list_item["completed"],
+                    todo_list_item["title"]
+                ])
 
-    # Write the header row to the CSV file.
-    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        print(f"TODO list for employee {employee_id} exported to {csv_file_path}.")
 
-    # Iterate over the employee's TODO list items, and write each item to the CSV file.
-    for todo_list_item in todo_list_items:
-        csv_writer.writerow([
-            employee_id,
-            todo_list_item["username"],
-            todo_list_item["completed"],
-            todo_list_item["title"]
-        ])
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching TODO list for employee {employee_id}: {e}")
 
-    # Close the CSV file.
-    csv_file.close()
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-# Pass employee id on command line.
-employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
 
-# Export the employee's TODO list to a CSV file.
-export_employee_todo_list_to_csv(employee_id)
+    export_employee_todo_list_to_csv(employee_id)
